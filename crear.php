ef050c3c2275120bc31e616aa1daae6132d80e9b
Andrés Giraldo <31672123+agiraldodev@users.noreply.github.com>
2024-03-12 11:13:47 -0500

    Proyecto finalizado con Bootstrap OK

--- crear.php
+++ crear.php
@@ -1,7 +1,14 @@
 <?php include 'plantilla.php'; ?>
 
-<form action="guardar.php" method="POST">
-    Nombre: <input type="text" name="nombre"/><br/>
-    Precio: <input type="text" name="precio"/><br/>
-    <input type="submit" value="Guardar producto">
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4">
+            <form action="guardar.php" method="POST">
+                Nombre: <input class="form-control" type="text" name="nombre" /><br />
+                Precio: <input class="form-control" type="text" name="precio" /><br />
+                <input type="submit" class="btn btn-success" value="Guardar producto">
+            </form>
+        </div>
+    </div>
+
+</div>
--- editar.php
+++ editar.php
@@ -9,9 +9,15 @@ $row = $result->fetch_assoc();
 
 ?>
 
-<form action="actualizar.php" method="POST">
-    <input type="hidden" name="id" value="<?php echo $row['id']; ?>"/>
-    Nombre: <input type="text" name="nombre" value="<?php echo $row['nombre']; ?>" />
-    Precio: <input type="text" name="precio" value="<?php echo $row['precio']; ?>" />
-    <input type="submit" value="Actualizar">
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4">
+            <form action="actualizar.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
+                Nombre: <input class="form-control" type="text" name="nombre" value="<?php echo $row['nombre']; ?>" />
+                Precio: <input class="form-control" type="text" name="precio" value="<?php echo $row['precio']; ?>" />
+                <input class="btn btn-primary" type="submit" value="Actualizar">
+            </form>
+        </div>
+    </div>
+</div>
