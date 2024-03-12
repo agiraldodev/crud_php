164ab41755df3f607971f1054fff6d2d313c839a
Andrés Giraldo <31672123+agiraldodev@users.noreply.github.com>
2024-03-12 09:36:35 -0500

    Formulario Crear Nuevo Producto

---
+++ crear.php
@@ -0,0 +1,5 @@
+<form action="guardar.php" method="POST">
+    Nombre: <input type="text" name="nombre"/><br/>
+    Precio: <input type="text" name="precio"/><br/>
+    <input type="submit" value="Guardar producto">
+</form>
