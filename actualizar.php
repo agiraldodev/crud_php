48dfa1833fb098f5b48d89c142960f062d7262da
Andrés Giraldo <31672123+agiraldodev@users.noreply.github.com>
2024-03-12 10:46:11 -0500

    Actualizar producto OK

---
+++ actualizar.php
@@ -0,0 +1,16 @@
+<?php
+include 'config.php';
+
+$id = $_POST['id'];
+$nombre = $_POST['nombre'];
+$precio = $_POST['precio'];
+
+$sql = "UPDATE productos SET nombre='$nombre', precio='$precio' WHERE id=$id";
+
+if($conn->query($sql) === TRUE){
+    header("Location: index.php");
+}else {
+    echo "Error al actualizar el producto: " . $sql . "<br/>" . $conn->error;
+}
+
+$conn->close();
